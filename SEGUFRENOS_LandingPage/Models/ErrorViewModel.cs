60a40ce4a7828a66dce2291576739dea429bc0fc
Usuario <Usuario@DESKTOP-IQE8EL8>
2026-08-02 21:07:25 -0500

    Agregar archivos de proyecto.

---
+++ SEGUFRENOS_LandingPage/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SEGUFRENOS_LandingPage.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
